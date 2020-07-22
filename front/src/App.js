c62376e8d968307636401251e275cc87c66fcba7
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-07-22 11:48:29 +0200

    mise en place de react router

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, NavLink } from 'react-router-dom'
+import Home from './components/Home'
+import ProjectsList from './components/ProjectsList'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <ul className='header'>
+        <li><NavLink activeClassName='active' exact to='/'>Home</NavLink></li>
+        <li><NavLink activeClassName='active' to='/projects'>Projects</NavLink></li>
+      </ul>
+
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/projects' component={ProjectsList} />
+        {/* <Route exact path='/project/:id' component={Project} /> */}
+      </Switch>
+
       <p>coucou</p>
     </div>
   );
